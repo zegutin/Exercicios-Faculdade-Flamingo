c04fd119d2052410d8e367f05b0445e7fee61798
Daniel Silva <105682016+zegutin@users.noreply.github.com>
2022-12-05 19:04:49 -0300

    Add files via upload

---
+++ tarefa03/Exemplo.java
@@ -0,0 +1,13 @@
+package tarefa02;
+
+import java.util.Scanner;
+
+public class Exemplo {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+	}
+
+}
